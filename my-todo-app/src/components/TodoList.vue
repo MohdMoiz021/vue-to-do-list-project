8c53eed6b15b3d9bc770fe4f89c769e03c0d5d9b
Mohd Moiz <mohdmoiz.3287@gmail.com>
2023-11-04 23:02:07 +0530

    first commit

---
+++ my-todo-app/src/components/TodoList.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+      <h1>To-Do List</h1>
+      <input v-model="newTodo" @keyup.enter="addTodo" placeholder="Add a task...">
+      <ul>
+        <li v-for="(todo, index) in todos" :key="index">
+          {{ todo }}
+          <button @click="removeTodo(index)">Remove</button>
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        todos: [],
+        newTodo: ''
+      };
+    },
+    methods: {
+      addTodo() {
+        if (this.newTodo.trim() !== '') {
+          this.todos.push(this.newTodo);
+          this.newTodo = '';
+        }
+      },
+      removeTodo(index) {
+        this.todos.splice(index, 1);
+      }
+    }
+  };
+  </script>
+  
